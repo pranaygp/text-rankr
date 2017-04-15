d243db9a548ff64bfa1b50b2b3c6328f9a06af7f
Pranay Prakash <pranay.gp@gmail.com>
2017-04-15 11:34:04 -0500

    fixes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # text-rankr
+
+I just recently learnt how to code so I wanted to make a little github repo to show my progress :)
+
+```
+print("Hello World!")
+```
+
+Thanks for stopping by
+
+Also, I'm an ECE major, so please go easy on me. I'm still learning and I'm definitely not a god like @pranaygp 
--- app.py
+++ app.py
@@ -1,33 +1,12 @@
 import networkx, numpy, scipy
 from flask import Flask, request, jsonify
-from summa import summarizer
 app = Flask(__name__)
-
-@app.route('/summarize', methods=['GET'])
-def summarize():
-	ret = dict()
-	args = request.args.to_dict()
-
-	try:
-		old_text = args['text']
-		new_text = summarizer.summarize(old_text)
-		ret['old_text'] = old_text
-		ret['new_text'] = new_text
-		ret['status'] = True
-		ret['error'] = None
-	except Exception as e:
-		ret['old_text'] = None
-		ret['new_text'] = None
-		ret['status'] = False
-		ret['error'] = str(e)
-
-	return jsonify(ret)
 
 @app.route('/', methods=['GET'])
 def index():
 	ret = dict()
-	ret['endpoints'] = ['/summarize']
-	ret['status'] = True
+	ret['drawbacks'] = ["My name is Shank", "My mum gave birth to me"]
+	ret['benefits'] = None
 	return jsonify(ret)
 
 if __name__=='__main__':
